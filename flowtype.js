66fa431139d05b9ecb5d72c04a725761ff06f0c3
John Wilson <john@j-wilson.com>
2013-08-22 09:58:05 -0500

    License Update!
    Sorry for the poor initial choice of CC licensing.

--- flowtype.js
+++ flowtype.js
@@ -2,12 +2,12 @@
 * If you create a derivative, please leave this text intact:
 *
 * FlowType.JS 1.0
-* Copyright 2013, Simple Focus http://simplefocus.com/
+* Copyright (c) 2013, Simple Focus http://simplefocus.com/
 *
 * FlowType.JS by Simple Focus (http://simplefocus.com/)
-* is licensed under a Creative Commons Attribution-ShareAlike
-* 3.0 Unported License. Learn more about this license at:
-* http://creativecommons.org/licenses/by-sa/3.0/deed.en_US
+* is licensed under the MIT License. Read a copy of the
+* license in the LICENSE.txt file or at
+* http://choosealicense.com/licenses/mit
 *
 * Thanks to Giovanni Difeterici (http://www.gdifeterici.com/)
 */
